955f99b941cc3948c5c2b7793bc75defac7ae7cd
Gayan D.M.G. Dissanayake <gayan.dissanayake@synapsys.lk>
2024-09-18 21:43:20 +0530

    dev(finish)

--- src/pages/gallerypage/GalleryPage.jsx
+++ src/pages/gallerypage/GalleryPage.jsx
@@ -6,12 +6,9 @@ import fish1 from "../../assets/images/fish1.jpg";
 import sustainable from "../../assets/images/gallerypage/4.jpg";
 import Slider from "react-slick";
 import slider1 from "../../assets/images/gallerypage/1.jpg";
-
+import slider2 from "../../assets/images/gallerypage/2.jpeg";
 import slider3 from "../../assets/images/gallerypage/3.jpg";
-import slider4 from "../../assets/images/gallerypage/4.jpg";
-import slider5 from "../../assets/images/gallerypage/5.jpg";
 import slider6 from "../../assets/images/gallerypage/6.jpg";
-import slider7 from "../../assets/images/gallerypage/7.jpg";
 
 const galleryItems = [
   {
@@ -60,12 +57,10 @@ const GalleryPage = () => {
 
   const sliderImages = [
     { src: slider1, alt: "Slider 1" },
-
-    { src: slider3, alt: "Slider 3" },
-    { src: slider4, alt: "Slider 4" },
-    { src: slider5, alt: "Slider 5" },
+    { src: sustainable, alt: "Slider 2" },
+    { src: slider3, alt: "Slider 4" },
+    { src: slider2, alt: "Slider 3" },
     { src: slider6, alt: "Slider 6" },
-    { src: slider7, alt: "Slider 7" },
   ];
 
   return (
@@ -95,7 +90,7 @@ const GalleryPage = () => {
           </animated.div>
         ))}
       </section>
-      <section className="hero">
+      <section className="section_hero">
         <Slider {...sliderSettings}>
           {sliderImages.map((img, index) => (
             <div key={index}>
